496ab4ba56946a085643cc63003c05425f32d567
SadLoneWolf <djordjeandjic99@gmail.com>
2021-06-13 11:15:07 +0200

    Need to reorganize elements on the window

--- Scripts/main.py
+++ Scripts/main.py
@@ -7,22 +7,30 @@ def generatePass():
     symbols = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K",
                "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
                "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", ".","*","/","?"]
-    passLen = int(length.get())
+    passLen = length.get()
+    if passLen == 0:
+        print("No value detected")
+    else:
+        passLen = int(length.get())
     for i in range(passLen):
        char = random.randint(0,len(symbols))
        print(symbols[char])
 
-#TODO Make a text field where you will show your password
-#TODO Add labels
-#TODO Reorganize elements on the window
+
 window = tk.Tk()
 window.title("Password Generator")
 window.geometry('400x300')
 window.resizable(False, False)
 length = tk.Entry(window,width = 2)
-length.pack(side=LEFT)
+length.pack()
 button = tk.Button(window, command=lambda: generatePass(), text="Generate")
 button.pack(side=BOTTOM)
-
+#TODO Make a text field where you will show your password
+field = tk.Entry(window,width = 24)
+field.pack()
+#TODO Add labels
+mainLabel = Label(window,anchor = CENTER,text = "Enter the number of charachters")
+mainLabel.pack()
+#TODO Reorganize elements on the window
 
 window.mainloop()
